cfcb009eb3f163f3ac0213bd024752ed66166293
Hector Hernandez <mine_291199@hotmail.com>
2023-02-10 18:48:30 -0600

    Se agrego el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainComponent } from './components/main/main.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'fir-auth-a2542',
+    appId: '1:574032225085:web:e68cb532615c4299315b54',
+    storageBucket: 'fir-auth-a2542.appspot.com',
+    apiKey: 'AIzaSyD3L07Z9SwbgQC4YX9DtZKEPFiV8q1CP34',
+    authDomain: 'fir-auth-a2542.firebaseapp.com',
+    messagingSenderId: '574032225085',
+  },
+    production: true
+  };
+  
